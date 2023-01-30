e4d7b4987aa5dccc634413e24f5fbd3da0b509a9
Corey <reg@pathrider.ca>
2023-01-30 08:55:35 -0700

    Misc fixes

--- src/helpers/filterByIndex.js
+++ src/helpers/filterByIndex.js
@@ -4,10 +4,12 @@ import { isPositiveInteger } from './dataTypes';
 const filterByIndex = (items: Array<any>, queryStr: string): Array<any> => {
   let itemsFiltered = [];
 
-  if ( !isNaN( queryStr )) {
+  // test if invalid number to search for
+  if ( isNaN( queryStr )) {
     return itemsFiltered;
   }
 
+  // test if positive number (valid index)
   if (isPositiveInteger(queryStr)) {
     itemsFiltered = items.filter(({ id }) => (
       Number(id) === Number(queryStr)
