b1330a9492324e23ce59d05b3b6491ae21d80bea
elhardoum <elhardoum@outlook.com>
2023-07-01 09:49:36 -0600

    fix preview mode link location

--- src/components/utils.js
+++ src/components/utils.js
@@ -3,7 +3,7 @@ export const redirectToPlayer =
   (mapId: number, nodeId: number): Function =>
   (): void => {
     window.open(
-      `${process.env.PLAYER_PUBLIC_URL}/olab/play#${mapId}:${nodeId}`,
+      `${process.env.PLAYER_PUBLIC_URL}/${mapId}/${nodeId}`,
     );
   };
 
