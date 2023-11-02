d1299b3b978204a8c4910b4ceffb78d3fbfa323a
Corey <reg@pathrider.ca>
2023-11-02 16:15:57 -0600

    setting change

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -x 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied: release | debug"
+    exit -1
+fi
 git pull
+if [ ! -L "build" ]; then
+        ln -s /var/www/vhosts/olab46/$1/player$1 build
+fi
 npm install
 npm run build
 service nginx restart
