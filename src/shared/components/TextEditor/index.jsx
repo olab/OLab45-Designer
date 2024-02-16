47103ee1f3d295cb11d0a2815d0086ed03653df7
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2024-02-16 11:31:28 -0700

    added process.env log

--- src/shared/components/TextEditor/index.jsx
+++ src/shared/components/TextEditor/index.jsx
@@ -25,9 +25,9 @@ class TextEditor extends React.Component {
     } = this.props;
 
     // handle case where tinymce is directly off the root
-    let url = '/tinymce/tinymce.min.js';
+    let url = '/tinymce/tinymce.js';
     if (process.env.PUBLIC_URL != '/') {
-      url = `${process.env.PUBLIC_URL}/tinymce/tinymce.min.js`;
+      url = `${process.env.PUBLIC_URL}/tinymce/tinymce.js`;
     }
 
     return (
