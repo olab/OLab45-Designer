8bc5368db777934ec70a742d5bf6b96ba5833663
Corey Wirun <reg@pathrider.ca>
2024-03-14 15:22:39 -0600

    TinyMCE edit config changes

--- src/components/AdvancedNodeEditor/MainTab/index.jsx
+++ src/components/AdvancedNodeEditor/MainTab/index.jsx
@@ -38,6 +38,20 @@ const MainTab = ({
     { label: 'Visit Once', value: isVisitOnce, name: 'isVisitOnce' },
   ];
 
+  const editorOptions = {
+    menubar: true,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'code | undo redo | formatselect | ' +
+    'bold italic image backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  };
+
   const positionInfo = `Position: (${Math.round(x)}, ${Math.round(y)})`;
   const idInfo = `Id: ${id}`;
 
@@ -59,6 +73,7 @@ const MainTab = ({
           text={text}
           handleEditorChange={handleEditorChange}
           handleKeyDown={handleKeyDown}
+          editorOptions={editorOptions}
         />
       </TextContent>
       <OtherContent>
