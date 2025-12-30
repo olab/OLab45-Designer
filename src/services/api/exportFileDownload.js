6900d1d86126a3287a42128c73032e6ecb6260fb
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2025-12-30 14:50:02 -0700

    removed extra function

--- src/services/api/exportFileDownload.js
+++ src/services/api/exportFileDownload.js
@@ -2,14 +2,6 @@ import createInstance from '../createCustomInstance';
 
 const API = createInstance();
 
-export function getFilenameFromHeaders(headers) {
-  const disposition = headers['content-disposition'];
-  if (!disposition) return null;
-
-  const match = disposition.match(/filename="?([^"]+)"?/);
-  return match ? match[1] : null;
-};
-
 export async function exportFileDownload(mapId) {
   try {
     const response = await API.get(
